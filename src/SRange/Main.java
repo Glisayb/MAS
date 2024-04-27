de6a1e4c7bbd2fe3293c3b3c7a55b67ed4919c6a
Glisayb <omenbaku@gmail.com>
2024-04-27 15:05:57 +0200

    wersja z maca last

--- src/SRange/Main.java
+++ src/SRange/Main.java
@@ -7,17 +7,23 @@ public class Main {
 
         System.out.println("Hello world!");
         ArrayList<Track> tracks = new ArrayList<>();
-        Address address = new Address("Warsaw","Waldorffa","23");
-        Range w23 = new Range(address,25,5);
+        Address address1 = new Address("Warsaw","Waldorffa","23");
+        Address address2 = new Address("Suchodol","Suchodolska","1");
+        Range w23 = new Range(address1,25,5);
+
         System.out.println(w23.numOfBelts());
         w23.setTrackDistance(2,13);
         System.out.println(w23.getGeoLocal());
         w23.setGeoLocal(21,37);
         System.out.println(w23.getGeoLocal());
 
-
         System.out.println(Range.getLongestDistance());
         System.out.println(w23.getAddress().toString());
         System.out.println(w23.getAvgDistance());
+
+        Range s1 = new Range(address1,300,2);
+
+        System.out.println(Range.getLongestDistance());
+
     }
 }
